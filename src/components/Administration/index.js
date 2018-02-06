ba445f335e41ee907efe55f9ad1e16179b74bb33
MAGM\Mario <mariogtzmdza@gmail.com>
2018-02-05 20:20:20 -0600

    create client completed

--- src/components/Administration/index.js
+++ src/components/Administration/index.js
@@ -10,25 +10,77 @@ class Admin extends Component {
         super();
 
         this.state = {
-            newClient: {}
+            newClient: {
+                
+            }
         }
+
+        //con bind se optiene todo el objeto this de la clase
+        this.handleInputChanged = this.handleInputChanged.bind(this);
+        this.postClient = this.postClient.bind(this);
     }
 
     componentDidMount() {
-     //   const urlProduct = 'http://localhost:3000/api/product/';
+        //   const urlProduct = 'http://localhost:3000/api/product/';
     }
 
-    postClient() {
+    postClient(e) {
         const urlClient = 'http://localhost:3000/api/client/';
         superagent
             .post(urlClient)
+            .set('Accept', 'application/json')
             .send(this.state.newClient)
             .then(res => {
-                console.log(res.body);
-                this.setState({
-                    data: res.body
-                })
+                console.log('NewClient', res.body);
             }).catch(err => console.log(err));
+    }
+
+    handleInputChanged(e) {
+        if (e.target.id === 'nameC') {
+            this.setState({
+                newClient: {
+                    name: e.target.value
+                }
+            });
+        }
+        if (e.target.id === 'lastnameFC') {
+            this.setState({
+                newClient: {
+                    name: this.state.newClient.name,
+                    lastnamefa: e.target.value
+                }
+            });
+        }
+        if (e.target.id === 'lastnameMC') {
+            this.setState({
+                newClient: {
+                    name: this.state.newClient.name,
+                    lastnamefa: this.state.newClient.lastnamefa,
+                    lastnamemo: e.target.value
+                }
+            });
+        }
+        if (e.target.id === 'birthdateC') {
+            this.setState({
+                newClient: {
+                    name: this.state.newClient.name,
+                    lastnamefa: this.state.newClient.lastnamefa,
+                    lastnamemo: this.state.newClient.lastnamemo,
+                    birthdate: e.target.value
+                }
+            });
+        }
+        if (e.target.id === 'addressC') {
+            this.setState({
+                newClient: {
+                    name: this.state.newClient.name,
+                    lastnamefa: this.state.newClient.lastnamefa,
+                    lastnamemo: this.state.newClient.lastnamemo,
+                    birthdate: this.state.newClient.birthdate,
+                    address: e.target.value
+                }
+            });
+        }
     }
 
     render() {
@@ -65,7 +117,7 @@ class Admin extends Component {
                         <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div className="modal-dialog" role="document">
                                 <div className="modal-content">
-                                    <div className="modal-header">
+                                    <div className="modal-header modal-color-success">
                                         <h5 className="modal-title" id="exampleModalLabel">Create a client</h5>
                                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
@@ -75,33 +127,31 @@ class Admin extends Component {
 
                                         <form>
                                             <div className="form-group">
-                                                <label for="nameClient">First Name</label>
-                                                <input type="text" className="form-control" id="nameClient" value={this.state.newClient.name} aria-describedby="nameClient" placeholder="Enter first name"/>
+                                                <label for="nameC">First Name</label>
+                                                <input type="text" className="form-control input-green" id="nameC" value={this.state.newClient.name} onChange={this.handleInputChanged} placeholder="Enter first name" />
                                             </div>
                                             <div className="form-group">
-                                                <label for="nameClient">Father's Last Name </label>
-                                                <input type="text" className="form-control" id="nameClient" value={this.state.newClient.lastnamefa} aria-describedby="nameClient" placeholder="Enter a father's lastname"/>
+                                                <label for="lastnameFC">Father's Last Name </label>
+                                                <input type="text" className="form-control input-green" id="lastnameFC" value={this.state.newClient.lastnamefa} onChange={this.handleInputChanged} placeholder="Enter a father's lastname" />
                                             </div>
                                             <div className="form-group">
-                                                <label for="nameClient">Mother's Last Name</label>
-                                                <input type="text" className="form-control" id="nameClient" value={this.state.newClient.lastnamemo} aria-describedby="nameClient" placeholder="Enter mother's last name"/>
+                                                <label for="lastnameMC">Mother's Last Name</label>
+                                                <input type="text" className="form-control input-green" id="lastnameMC" value={this.state.newClient.lastnamemo} onChange={this.handleInputChanged} placeholder="Enter mother's last name" />
                                             </div>
                                             <div className="form-group">
-                                                <label for="nameClient">Birthdate</label>
-                                                <input type="text" className="form-control" id="nameClient" value={this.state.newClient.birthdate} aria-describedby="nameClient" placeholder="Enter a birthdate"/>
+                                                <label for="birthdateC">Birthdate</label>
+                                                <input type="text" className="form-control input-green" id="birthdateC" value={this.state.newClient.birthdate} onChange={this.handleInputChanged} placeholder="Enter  YYYY-MM-DD" />
                                             </div>
                                             <div className="form-group">
-                                                <label for="nameClient">Address</label>
-                                                <input type="text" className="form-control" id="nameClient" value={this.state.newClient.address} aria-describedby="nameClient" placeholder="Enter an address"/>
+                                                <label for="addressC">Address</label>
+                                                <input type="text" className="form-control input-green" id="addressC" value={this.state.newClient.address} onChange={this.handleInputChanged} placeholder="Enter an address" />
                                             </div>
-                                            
-                                            
                                         </form>
 
                                     </div>
-                                    <div className="modal-footer">
+                                    <div className="modal-footer modal-color-success">
                                         <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-                                        <button onClick={this.handleResultClick} type="submit" className="btn btn-outline-success">Save changes</button>
+                                        <button onClick={this.postClient} id="newClientMade" className="btn btn-outline-success">Save</button>
                                     </div>
                                 </div>
                             </div>
